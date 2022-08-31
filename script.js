d3ad81e6a08c8c40d21bce1fb8ebed1272096399
Josepch1 <josepedro.homenhuck@gmail.com>
2022-08-31 18:03:26 -0300

    modal

---
+++ script.js
@@ -0,0 +1,9 @@
+function acaoCalculadora(){
+    let modal = document.querySelector('.modalCalculadora')
+    modal.style.display = 'block';
+}
+
+function fechar(){
+    let modal = document.querySelector('.modalCalculadora')
+    modal.style.display = 'none';
+}
