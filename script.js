4e96422cb9f165e7364726b8b1041ac02ac7ebb1
Josepch1 <josepedro.homenhuck@gmail.com>
2022-08-31 19:07:47 -0300

    dale

--- script.js
+++ script.js
@@ -3,7 +3,47 @@ function acaoCalculadora(){
     modal.style.display = 'block';
 }
 
-function fechar(){
+function fecharCalculadora(){
     let modal = document.querySelector('.modalCalculadora')
+    modal.style.display = 'none';
+}
+
+function acaoLista(){
+    let modal = document.querySelector('.modalLista')
+    modal.style.display = 'block';
+}
+
+function fecharLista(){
+    let modal = document.querySelector('.modalLista')
+    modal.style.display = 'none';
+}
+
+function acaoTermo(){
+    let modal = document.querySelector('.modalTermo')
+    modal.style.display = 'block';
+}
+
+function fecharTermo(){
+    let modal = document.querySelector('.modalTermo')
+    modal.style.display = 'none';
+}
+
+function acaoCharada(){
+    let modal = document.querySelector('.modalCharada')
+    modal.style.display = 'block';
+}
+
+function fecharCharada(){
+    let modal = document.querySelector('.modalCharada')
+    modal.style.display = 'none';
+}
+
+function acaoUpgolden(){
+    let modal = document.querySelector('.modalUpgolden')
+    modal.style.display = 'block';
+}
+
+function fecharUpgolden(){
+    let modal = document.querySelector('.modalUpgolden')
     modal.style.display = 'none';
 }
