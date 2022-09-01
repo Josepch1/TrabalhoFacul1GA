459cf835cc96a7267ae923ece15dfdf7c10e82d4
Josepch1 <josepedro.homenhuck@gmail.com>
2022-09-01 10:35:00 -0300

    main

--- script.js
+++ script.js
@@ -1,49 +1,49 @@
-function acaoCalculadora(){
-    let modal = document.querySelector('.modalCalculadora')
-    modal.style.display = 'block';
+function acaoCalculadora() {
+  let modal = document.querySelector(".modalCalculadora");
+  modal.style.display = "block";
 }
 
-function fecharCalculadora(){
-    let modal = document.querySelector('.modalCalculadora')
-    modal.style.display = 'none';
+function fecharCalculadora() {
+  let modal = document.querySelector(".modalCalculadora");
+  modal.style.display = "none";
 }
 
-function acaoLista(){
-    let modal = document.querySelector('.modalLista')
-    modal.style.display = 'block';
+function acaoLista() {
+  let modal = document.querySelector(".modalLista");
+  modal.style.display = "block";
 }
 
-function fecharLista(){
-    let modal = document.querySelector('.modalLista')
-    modal.style.display = 'none';
+function fecharLista() {
+  let modal = document.querySelector(".modalLista");
+  modal.style.display = "none";
 }
 
-function acaoTermo(){
-    let modal = document.querySelector('.modalTermo')
-    modal.style.display = 'block';
+function acaoTermo() {
+  let modal = document.querySelector(".modalTermo");
+  modal.style.display = "block";
 }
 
-function fecharTermo(){
-    let modal = document.querySelector('.modalTermo')
-    modal.style.display = 'none';
+function fecharTermo() {
+  let modal = document.querySelector(".modalTermo");
+  modal.style.display = "none";
 }
 
-function acaoCharada(){
-    let modal = document.querySelector('.modalCharada')
-    modal.style.display = 'block';
+function acaoCharada() {
+  let modal = document.querySelector(".modalCharada");
+  modal.style.display = "block";
 }
 
-function fecharCharada(){
-    let modal = document.querySelector('.modalCharada')
-    modal.style.display = 'none';
+function fecharCharada() {
+  let modal = document.querySelector(".modalCharada");
+  modal.style.display = "none";
 }
 
-function acaoUpgolden(){
-    let modal = document.querySelector('.modalUpgolden')
-    modal.style.display = 'block';
+function acaoUpgolden() {
+  let modal = document.querySelector(".modalUpgolden");
+  modal.style.display = "block";
 }
 
-function fecharUpgolden(){
-    let modal = document.querySelector('.modalUpgolden')
-    modal.style.display = 'none';
+function fecharUpgolden() {
+  let modal = document.querySelector(".modalUpgolden");
+  modal.style.display = "none";
 }
